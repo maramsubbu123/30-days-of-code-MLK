debc5ce381131a699259eda596492e9d35512a1e
maramsubbu123 <84636793+maramsubbu123@users.noreply.github.com>
2022-05-04 21:08:34 +0530

    Create Day9_1DArray.cpp

---
+++ Subrahmanyam/Day9_1DArray.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int find_largest(int nums[], int n) {
+  return *max_element(nums, nums + n);
+}
+
+int main() {
+  int nums[] = {5,4,9,22,8};
+  int n = sizeof(nums) / sizeof(nums[0]);
+    cout << "Original array:";
+    for (int i=0; i < n; i++) 
+    cout << nums[i] <<" ";
+    
+  cout << "\nLargest element of the said array: "<< find_largest(nums, n);
+  return 0;
+}
